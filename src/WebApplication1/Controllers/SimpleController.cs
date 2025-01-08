05e57f940971438ccec836cbe0ef98351adb5017
admin <111@111.ru>
2025-01-08 21:10:42 +0300

    4. Expose the simple operation via a Web API

---
+++ src/WebApplication1/Controllers/SimpleController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using testTask;
+
+namespace WebApplication1.Controllers;
+
+[ApiController]
+[Route("/[controller]")]
+public class SimpleController : ControllerBase
+{
+    /// <summary>
+    /// sum two simple ints. no overload check
+    /// </summary>
+    [HttpGet]
+    public async Task<int> GetSumAsync(int a, int b)
+    {
+        return Interop.Sum(a, b);
+    }
+}
